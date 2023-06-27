6727239d65a2302c3867192d9dfaf2a5e1bf0024
Josh Cole <colemangrill@live.com>
2023-06-27 06:50:31 -0700

    update clang-format rules

--- src/algen.cpp
+++ src/algen.cpp
@@ -7,96 +7,97 @@
 
 namespace algen {
 
-namespace internal {
+    namespace internal {
 
-template <class T>
-bool sort(Node<T>& a, Node<T>& b) {
-    return a.score > b.score;
-}
-
-template <class Solution, class FeatureFlags>
-Node<Solution> tournamentSelection(Node<Solution>* nodes, unsigned int nodeLength, Parameters<FeatureFlags> params) {
-    Node<Solution> bestNode;
-    float bestScore = 0.0;
-
-    for (int i = 0; i < params.tournamentSize; i++) {
-        int idx = rand() % nodeLength;
-        Node<Solution> node = nodes[idx];
-
-        if (node.score > bestScore) {
-            bestScore = node.score;
-            bestNode = node;
+        template <class T>
+        bool sort(Node<T>& a, Node<T>& b) {
+            return a.score > b.score;
         }
-    }
 
-    return bestNode;
-}
+        template <class Solution, class FeatureFlags>
+        Node<Solution> tournamentSelection(Node<Solution>* nodes, unsigned int nodeLength,
+                                           Parameters<FeatureFlags> params) {
+            Node<Solution> bestNode;
+            float bestScore = 0.0;
 
-}  // namespace internal
+            for (int i = 0; i < params.tournamentSize; i++) {
+                int idx = rand() % nodeLength;
+                Node<Solution> node = nodes[idx];
 
-template <class InputData, class OutputData, class Solution, class FeatureFlags>
-Node<Solution> runAlgorithm(Parameters<FeatureFlags> params, InputData input,
-                            Algorithm<InputData, OutputData, Solution, FeatureFlags>* algorithm,
-                            Analyzer<OutputData, FeatureFlags>* analyzer) {
-    // Define generational parameters
-    int poplen = params.population;
-    Node<Solution> population[poplen];
-    Node<Solution> nextPopulation[poplen];
-    OutputData outputs[poplen];
+                if (node.score > bestScore) {
+                    bestScore = node.score;
+                    bestNode = node;
+                }
+            }
 
-    // Best solutions
-    float bestScore = 0.0;
-    Node<Solution> bestSolution;
-    OutputData bestOutput;
+            return bestNode;
+        }
 
-    for (int i = 0; i < poplen; i++) {
-        population[i] = algorithm->allocateNode(input, params);
-    }
+    }  // namespace internal
 
-    for (int generation = 0; generation < params.generations; generation++) {
-        int nextPopIter = 0;
-        std::cout << "Starting generation " << generation << std::endl;
+    template <class InputData, class OutputData, class Solution, class FeatureFlags>
+    Node<Solution> runAlgorithm(Parameters<FeatureFlags> params, InputData input,
+                                Algorithm<InputData, OutputData, Solution, FeatureFlags>* algorithm,
+                                Analyzer<OutputData, FeatureFlags>* analyzer) {
+        // Define generational parameters
+        int poplen = params.population;
+        Node<Solution> population[poplen];
+        Node<Solution> nextPopulation[poplen];
+        OutputData outputs[poplen];
 
-        // Evaluate all the solutions
-        for (int r = 0; r < poplen; r++) {
-            outputs[r] = algorithm->generateOutput(population[r], input, params);
-            population[r].score = analyzer->score(outputs[r], params);
+        // Best solutions
+        float bestScore = 0.0;
+        Node<Solution> bestSolution;
+        OutputData bestOutput;
 
-            if (population[r].score > bestScore) {
-                bestScore = population[r].score;
-                bestSolution = population[r];
-                bestOutput = outputs[r];
+        for (int i = 0; i < poplen; i++) {
+            population[i] = algorithm->allocateNode(input, params);
+        }
+
+        for (int generation = 0; generation < params.generations; generation++) {
+            int nextPopIter = 0;
+            std::cout << "Starting generation " << generation << std::endl;
+
+            // Evaluate all the solutions
+            for (int r = 0; r < poplen; r++) {
+                outputs[r] = algorithm->generateOutput(population[r], input, params);
+                population[r].score = analyzer->score(outputs[r], params);
+
+                if (population[r].score > bestScore) {
+                    bestScore = population[r].score;
+                    bestSolution = population[r];
+                    bestOutput = outputs[r];
+                }
+            }
+
+            // Create the next population'
+            std::sort(population, population + (sizeof(population) / sizeof(population[0])),
+                      algen::internal::sort<Solution>);
+
+            // Take the creame of the crop, in both directions. And we multiply by 0.5 because
+            // each iteration takes 2 nodes
+            for (; nextPopIter < poplen * params.elitismFactor; nextPopIter += 2) {
+                int bottomIdx = poplen - nextPopIter - 1;
+                int topIdx = nextPopIter;
+                nextPopulation[nextPopIter] = population[bottomIdx];
+                nextPopulation[nextPopIter + 1] = population[topIdx];
+            }
+
+            for (int r = 0; r < poplen - nextPopIter; r++) {
+                Node<Solution> left = internal::tournamentSelection(population, poplen, params);
+                Node<Solution> right = internal::tournamentSelection(population, poplen, params);
+
+                nextPopulation[nextPopIter++] = algorithm->combineNodes(left, right, params);
+            }
+
+            // Promote nextPopulation into real population
+            for (int r = 0; r < poplen; r++) {
+                population[r] = nextPopulation[r];
             }
         }
 
-        // Create the next population'
-        std::sort(population, population + (sizeof(population) / sizeof(population[0])),
-                  algen::internal::sort<Solution>);
-
-        // Take the creame of the crop, in both directions. And we multiply by 0.5 because
-        // each iteration takes 2 nodes
-        for (; nextPopIter < poplen * params.elitismFactor; nextPopIter += 2) {
-            int bottomIdx = poplen - nextPopIter - 1;
-            int topIdx = nextPopIter;
-            nextPopulation[nextPopIter] = population[bottomIdx];
-            nextPopulation[nextPopIter + 1] = population[topIdx];
-        }
-
-        for (int r = 0; r < poplen - nextPopIter; r++) {
-            Node<Solution> left = internal::tournamentSelection(population, poplen, params);
-            Node<Solution> right = internal::tournamentSelection(population, poplen, params);
-
-            nextPopulation[nextPopIter++] = algorithm->combineNodes(left, right, params);
-        }
-
-        // Promote nextPopulation into real population
-        for (int r = 0; r < poplen; r++) {
-            population[r] = nextPopulation[r];
-        }
+        std::cout << "Simulation terminated" << std::endl;
+        return bestSolution;
     }
-
-    std::cout << "Simulation terminated" << std::endl;
-    return bestSolution;
-}
 
 }  // namespace algen
--- src/algen.h
+++ src/algen.h
@@ -3,42 +3,42 @@
 
 namespace algen {
 
-template <class Solution>
-struct Node {
-    float score;
-    Solution solution;
-};
+    template <class Solution>
+    struct Node {
+        float score;
+        Solution solution;
+    };
 
-template <class Solution>
-struct Result {
-    float score;
-    Node<Solution> output;
-};
+    template <class Solution>
+    struct Result {
+        float score;
+        Node<Solution> output;
+    };
 
-template <class FeatureFlags>
-struct Parameters {
-    unsigned int generations;
-    unsigned int population;
-    float elitismFactor;
-    float crossoverFactor;
-    float mutationFactor;
-    unsigned int tournamentSize;
-    FeatureFlags featureFLags;
-};
+    template <class FeatureFlags>
+    struct Parameters {
+        unsigned int generations;
+        unsigned int population;
+        float elitismFactor;
+        float crossoverFactor;
+        float mutationFactor;
+        unsigned int tournamentSize;
+        FeatureFlags featureFLags;
+    };
 
-template <class InputData, class OutputData, class Solution, class FeatureFlags>
-class Algorithm {
-   public:
-    virtual OutputData generateOutput(Node<Solution> node, InputData input, Parameters<FeatureFlags> params);
-    virtual Node<Solution> allocateNode(InputData input, Parameters<FeatureFlags> params);
-    virtual Node<Solution> combineNodes(Node<Solution> left, Node<Solution> right, Parameters<FeatureFlags> params);
-};
+    template <class InputData, class OutputData, class Solution, class FeatureFlags>
+    class Algorithm {
+       public:
+        virtual OutputData generateOutput(Node<Solution> node, InputData input, Parameters<FeatureFlags> params);
+        virtual Node<Solution> allocateNode(InputData input, Parameters<FeatureFlags> params);
+        virtual Node<Solution> combineNodes(Node<Solution> left, Node<Solution> right, Parameters<FeatureFlags> params);
+    };
 
-template <class OutputData, class FeatureFlags>
-class Analyzer {
-   public:
-    virtual float score(OutputData attempt, Parameters<FeatureFlags> params);
-};
+    template <class OutputData, class FeatureFlags>
+    class Analyzer {
+       public:
+        virtual float score(OutputData attempt, Parameters<FeatureFlags> params);
+    };
 
 }  // namespace algen
 
