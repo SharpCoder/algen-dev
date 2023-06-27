a1409a91b1b7afb306603b363cc30a017323104a
Josh Cole <colemangrill@live.com>
2023-06-27 08:05:25 -0700

    Improve the initial hello_world app with comments

--- examples/hello_world.cpp
+++ examples/hello_world.cpp
@@ -10,26 +10,51 @@ using namespace algen;
 /** Structs needed for this genetic algorith */
 /*********************************************/
 
+// This represents the input for your problem. In our case,
+// the input is a randomized string of 13 characters.
 struct InputData {
     char seed[13];
 };
 
+// This represents one possible solution to your problem. In
+// our case this is an array of character shifts.
 struct Solution {
     int deltas[13];
 };
 
+// This represents the final output of a given solution. In
+// our case, given an array of shifts and an input data, we
+// will generate a final 13 character string as the output.
 struct OutputData {
     char final[13];
 };
 
+// This is an optional set of feature flags that can be passed
+// in to each function. Use it to configure your algorithm.
 struct FeatureFlags {};
 
+/*******************************************/
+/* Helpers                                 */
+/*******************************************/
 char target[13] = {'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '!'};
+int randomCharacter() { return (rand() % 128) - (rand() % 128); }
+void printNode(Node<Solution> node) {
+    std::cout << "node " << node.score << " [";
+    for (int i = 0; i < 13; i++) {
+        std::cout << node.solution.deltas[i] << " ";
+    }
+    std::cout << "]" << std::endl;
+}
 
 /*******************************************/
 /* Implementation of classes               */
 /*******************************************/
+
+// The Analyzer class is used to score a given solution
 class HelloAnalyzer : public Analyzer<OutputData, FeatureFlags> {
+    // Score a given solution by calculating how far off each letter
+    // is from the target and using the inverse difference as
+    // the score.
     float score(OutputData attempt, Parameters<FeatureFlags> params) {
         float score = 0.0;
         for (int i = 0; i < 13; i++) {
@@ -45,17 +70,13 @@ class HelloAnalyzer : public Analyzer<OutputData, FeatureFlags> {
     }
 };
 
-int randomCharacter() { return (rand() % 128) - (rand() % 128); }
-void printNode(Node<Solution> node) {
-    std::cout << "node " << node.score << " [";
-    for (int i = 0; i < 13; i++) {
-        std::cout << node.solution.deltas[i] << " ";
-    }
-    std::cout << "]" << std::endl;
-}
-
+// The Algorithm class is used to perform the actual genetic algorithm
+// logic. It's separate from the analyzer so you can create multiple
+// different ways to solve the problem and mix/match later.
 class HelloAlgorithm : public Algorithm<InputData, OutputData, Solution, FeatureFlags> {
    public:
+    // This method will take a solution, the input, the parameters, and evaluate
+    // what a final output would look like given those things.
     OutputData generateOutput(Node<Solution> node, InputData input, Parameters<FeatureFlags> params) {
         OutputData* result = new OutputData();
         for (int i = 0; i < 13; i++) {
@@ -64,6 +85,8 @@ class HelloAlgorithm : public Algorithm<InputData, OutputData, Solution, Feature
         return *result;
     }
 
+    // This method will create a brand new, empty state. It should be randomized
+    // values which are used to seed the initial population.
     Node<Solution> allocateNode(InputData input, Parameters<FeatureFlags> params) {
         Node<Solution>* node = new Node<Solution>();
         node->score = std::numeric_limits<float>::min();
@@ -75,6 +98,10 @@ class HelloAlgorithm : public Algorithm<InputData, OutputData, Solution, Feature
         return *node;
     }
 
+    // This method will take two solutions and try to create a better one using parts of each.
+    // There are many ways to implement this method, but in general, you should strive to
+    // implement crossover and mutation (at a minimum). Whatever that looks likef or your
+    // specific problem.
     Node<Solution> combineNodes(Node<Solution> left, Node<Solution> right, Parameters<FeatureFlags> params) {
         Node<Solution>* node = new Node<Solution>();
 
@@ -91,8 +118,8 @@ class HelloAlgorithm : public Algorithm<InputData, OutputData, Solution, Feature
 
         for (int i = 0; i < 13; i++) {
             // Mutation logic
-            if (rand() % 800 == 0) node->solution.deltas[i] = randomCharacter();
-            if (rand() % 800 == 0) node->solution.deltas[i] = randomCharacter();
+            if (rand() % 500 == 0) node->solution.deltas[i] = randomCharacter();
+            if (rand() % 500 == 0) node->solution.deltas[i] = randomCharacter();
 
             // Boundary logic
             if (node->solution.deltas[i] < -128) {
@@ -108,20 +135,29 @@ class HelloAlgorithm : public Algorithm<InputData, OutputData, Solution, Feature
 
 int main() {
     InputData input;
-    Parameters<FeatureFlags> params = {.generations = 300,
-                                       .population = 10000,
+
+    // Randomize the input to alpha characters
+    for (int i = 0; i < 13; i++) {
+        input.seed[i] = 'a' + rand() % 26;
+    }
+
+    // Setup the parameters for this experiment.
+    Parameters<FeatureFlags> params = {.generations = 500,
+                                       .population = 1000,
                                        .elitismFactor = 0.01,
                                        .crossoverFactor = 0.5,
                                        .mutationFactor = 0.001,
-                                       .tournamentSize = 10,
+                                       .tournamentSize = 5,
                                        .featureFLags = {}};
 
+    // Choose our analyzer and algorithm
     HelloAlgorithm algorithm;
     HelloAnalyzer analyzer;
 
-    Node<Solution> winner =
-        runAlgorithm<InputData, OutputData, Solution, FeatureFlags>(params, input, &algorithm, &analyzer);
+    // Execute the program.
+    Node<Solution> winner = runAlgorithm(params, input, &algorithm, &analyzer);
 
+    // Output the best solution we've found
     std::cout << "Winning output " << algorithm.generateOutput(winner, input, params).final << std::endl;
     return 0;
 }
